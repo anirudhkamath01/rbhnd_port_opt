760969380ced25cd21f41766d4e66020cff2f6d6
anirudhkamath01 <96703593+anirudhkamath01@users.noreply.github.com>
2021-12-28 10:15:00 -0800

    Update portfolio_optimization.py

--- portfolio_optimization.py
+++ portfolio_optimization.py
@@ -7,7 +7,6 @@ from pandas_datareader import data as web
 import numpy as np
 from datetime import datetime
 import matplotlib.pyplot as plt
-import PyPortfolioOpt as pypfopt
 import scipy.optimize as sc
 from pypfopt import plotting
 plt.style.use('fivethirtyeight')
